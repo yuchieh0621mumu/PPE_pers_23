7d6cab31589c4e53a91ccb3666b4a92bcbc3b420
yuchieh0621mumu <113918568+yuchieh0621mumu@users.noreply.github.com>
2023-10-19 16:41:10 +0200

    modified

--- TD3_bash/first_script_2.sh
+++ TD3_bash/first_script_2.sh
@@ -1,16 +1,27 @@
-#!/user/bin/bash
 
-#declare the variables
-WORD=$1   
-COUNT=$6
+#!/usr/bin/env bash
+
+#declare the variables, first command line argument is the word
+#set the count variable to 6 in order to count from 6 to 8
+WORD=$1
+COUNT=6
 MAX=8
 
-SCRIPT_PATH="./first_script.sh"
+# Indicate the path to the script file
+SCRIPT_PATH="./first_script_2.sh"
 
-#use the loop while with 6, 7, 8 and then run the file first_script.sh
-
-while [[ $COUNT -le $MAX ]];
-do
-        bash $SCRIPT_PATH $COUNT $WORD
-        COUNT=$((COUNT+1))
-done
+# Check whether the word variable is empty by using the -z syntax
+if [[ -z $WORD ]]; then
+  echo "Please enter the keyword: "
+else
+  # Use a while loop to iterate from 6 to 8 and run the script
+  #runs while count variables is less than or equal to max, if it equals to max(8) we'll break the loop
+  while [[ $COUNT -le $MAX ]]; do
+    if bash $SCRIPT_PATH $COUNT $WORD; then
+      echo "Script ran successfully for COUNT $COUNT"
+    else
+      echo "Script failed for COUNT $COUNT"
+    fi
+    COUNT=$((COUNT+1))
+  done
+fi
